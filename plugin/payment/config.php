5ee04003035fcd691e337781cae4595c11280e96
Eric.du <dukai7824@dingtalk.com>
2018-09-29 16:27:28 +0800

    upload demo

--- plugin/payment/config.php
+++ plugin/payment/config.php
@@ -1,14 +1,3 @@
 <?php
 
-    // DCpay
-    // define('DCPAY_MERCHANT_ACCOUNT','123456789'); //商户号
-	// define('DCPAY_APPKAY',' 0123456789abcdef0123456789abcdef'); //密钥
-	// define('DCPAY_NOTIFY_URL','https://'.SERVER_ADDR.'/dcpay_notify_url.php'); 
-	// define('DCPAY_RETURN_URL','https://'.SERVER_ADDR.'/dcpay_return_url.php'); 
-    // define('DCPAY_QUERY_URL','https://payment.ccp.boarding.dcpay.biz/ChinaDebitCard'); 
-    
-    define('DCPAY_MERCHANTID', '567654633');
-    define('DCPAY_PRODUCTKEY', '3ab3e5036c52934f3f9cf92cdeed01a5');//对幾T产体~Akey
-    define('DCPAY_QUERY_URL', 'https://payment.ccp.dcpay.biz/ChinaDebitCard');
-    define('DCPAY_NOTIFY_URL', 'http://verydows.cn/api/pay/notify/alipay');
-    define('DCPAY_RETURN_URL', 'http://verydows.cn/api/pay/notify/alipay');
+
